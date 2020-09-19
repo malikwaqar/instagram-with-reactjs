555e28ec0ab36babb95407128122c3d105f16cde
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-19 18:14:24 +0500

    Signup Added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,65 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Post from './components/Post/Post';
-import { db } from './firebase';
+import { db, auth } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const[modalStyle] = useState(getModalStyle);
   const[posts, setPosts] = useState([]);
+  const[open, setOpen] = useState(false);
+  const[username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+  const[email, setEmail] = useState('');
+  const[user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        setUser(authUser); 
+        if(authUser.displayName) {
+
+        }
+        else {
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      }
+      else {
+        setUser(null);
+      }
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [user, username]);
 
   useEffect (() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -15,8 +70,49 @@ function App() {
       })));
     })
   }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message) )
+  }
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+      <form className="app__signup">
+      <center>
+        <img className="header__logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="Instagram Logo" />
+      </center>
+      <Input
+      placeholder="username"
+      type="text"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+      placeholder="email"
+      type="text"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+      placeholder="password"
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" onClick={signUp}>Sign Up</Button>
+      
+      </form>
+      
+    </div>
+      </Modal>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <Header />
       {
         posts.map(({id, post}) => (
